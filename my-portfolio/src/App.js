ab165de50bbbe04fecb3111e40adcb810a83dc34
Estibaliz Barato <estibalizbarato@gmail.com>
2021-03-12 19:48:18 +0100

    install react router and add structure components

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,7 +1,19 @@
+import React from "react";
+import { Switch } from "react-router";
 import "./App.css";
+// components
+import Index from "./components/Index";
+import Landing from "./components/Landing";
 
 function App() {
-  return <div className="App">hola mundo</div>;
+  return (
+    <>
+      <Switch>
+        <Landing exact path="/estibalizBarato" />
+        <Index path="/index" />
+      </Switch>
+    </>
+  );
 }
 
 export default App;
