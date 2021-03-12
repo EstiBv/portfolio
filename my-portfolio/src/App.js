5c238404ca6f288dcbb7086048b0558329a92be0
Estibaliz Barato <estibalizbarato@gmail.com>
2021-03-12 19:51:24 +0100

    fix routes

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { Switch } from "react-router";
+import { Route, Switch } from "react-router";
 import "./App.css";
 // components
 import Index from "./components/Index";
-import Landing from "./components/Landing";
+import Home from "./components/Home";
 
 function App() {
   return (
     <>
       <Switch>
-        <Landing exact path="/estibalizBarato" />
-        <Index path="/index" />
+        <Route exact path="/" component={Home} />
+        <Route path="/index" component={Index} />
       </Switch>
     </>
   );
