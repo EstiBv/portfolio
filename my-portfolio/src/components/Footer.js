e2a097f676ba451f8d833a500fa3796fd02222b4
Estibaliz Barato <estibalizbarato@gmail.com>
2021-03-28 13:39:15 +0200

    add footer styles

--- my-portfolio/src/components/Footer.js
+++ my-portfolio/src/components/Footer.js
@@ -1,19 +1,70 @@
 import React from "react";
-
+import "../stylesheets/footer.scss";
 const Footer = () => {
   return (
-    <footer>
-      <nav>
-        <a href="https://www.notion.com">
-          Notion
-          <img src="../../assets/not-nicons.svg" alt="" />
-        </a>
-        <a href="https://github.com/EstiBv"> GitHub</a>
-        <a href="estibalizbarato@gmail.com"> Gmail</a>
-        <a href="https://www.linkedin.com/in/estibalizbarato/"> Linkedin</a>
-        <a href="estibalizbarato@gmail.com"> Behance</a>
+    <footer className="footer">
+      <nav className="footer__menu">
+        <ul className="footer__menuList">
+          <li className="footer__menuList-notion">
+            <a
+              href="https://www.notion.com"
+              target="_blank"
+              rel="noreferrer"
+              title="access Notion"
+              className="footer__menuList-into"
+            >
+              {" "}
+            </a>
+          </li>
+          <li className="footer__menuList-gitHub">
+            <a
+              href="https://github.com/EstiBv"
+              target="_blank"
+              rel="noreferrer"
+              title="access gitHub"
+              className="footer__menuList-into"
+            >
+              {" "}
+            </a>
+          </li>
+          <li className="footer__menuList-email">
+            <a
+              href="mailto: estibalizbarato@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+              title="send email"
+              className="footer__menuList-into"
+            >
+              {" "}
+            </a>
+          </li>
+          <li className="footer__menuList-linkedin">
+            <a
+              href="https://www.linkedin.com/in/estibalizbarato/"
+              target="_blank"
+              rel="noreferrer"
+              title="access linkedin"
+              className="footer__menuList-into"
+            >
+              {" "}
+            </a>
+          </li>
+          <li className="footer__menuList-behance">
+            <a
+              href="https://www.behance.net/estibalizbarato"
+              target="_blank"
+              rel="noreferrer"
+              title="access behance"
+              className="footer__menuList-into"
+            >
+              {" "}
+            </a>
+          </li>
+        </ul>
       </nav>
-      <small>© 2021 por Estíbaliz Barato</small>
+      <small className="footer__copy">
+        <span>© 2021 por Estíbaliz Barato</span>
+      </small>
     </footer>
   );
 };
