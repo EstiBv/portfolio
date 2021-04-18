886f29370162f10cf0a05b0adb98d3fa96ad1e75
Estibaliz Barato <estibalizbarato@gmail.com>
2021-04-18 13:51:07 +0200

    add styles into Develop component

--- my-portfolio/src/components/indexProjects/Develop.js
+++ my-portfolio/src/components/indexProjects/Develop.js
@@ -1,9 +1,50 @@
 import React from "react";
 import InfoDevelop from "./infographics/InfoDevelop";
 import "../../stylesheets/pages/develop.scss";
+import Header from "../Header";
+import Footer from "../Footer";
 
 const Develop = () => {
-  return <InfoDevelop />;
+  return (
+    <>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <article>
+          <h2>
+            <span>33 </span>
+            lenguaje
+          </h2>
+          <h2>
+            <span>22 </span>
+            stack
+          </h2>
+          <h2>
+            <span>11 </span>
+            herramientas
+          </h2>
+          <figure>
+            <InfoDevelop />
+          </figure>
+        </article>
+        <section>
+          <div className="backgroundImg">
+            <button className="button1">buton1</button>
+            <button className="button2">button2</button>
+            <div className="backgroundImg2">IMG</div>
+          </div>
+          <article className="imgDescription">
+            <h4>titulo</h4>
+            <p>Descripcion</p>
+          </article>
+        </section>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </>
+  );
 };
 
 export default Develop;
